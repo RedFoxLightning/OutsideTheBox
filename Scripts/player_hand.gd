32ff8befb6392f3ac947b1457497f7a55c29eb78
RedFoxLightning <57427142+RedFoxLightning@users.noreply.github.com>
2026-03-01 20:31:59 -0800

    Light cleanup + tweaks(end of day1)
    Added animations for grabbing and panning, changed the hand size and camera size, lowered the panning sensitivity to 1, cleaned up unnused variables, and renamed stuff in the inspector to have better names

--- Scripts/player_hand.gd
+++ Scripts/player_hand.gd
@@ -2,26 +2,48 @@ extends Node2D
 
 const maxDistanceFromMouse: float = 50
 const handStiffness: float = 3
-const panning_sensitivity: float = 3
+const panning_sensitivity: float = 1 # 1 means it'll move perfectly with the mouse, 2 means it'll move double
 
 @onready var camera: Camera2D = %Camera2D
+@onready var animator: AnimatedSprite2D = $AnimatedSprite2D
+
+var grabbing: bool
+
 var mousePosition: Vector2
-var mousePositionLastFrame: Vector2
-var positionLastFrame: Vector2
-var dragging: bool
+var panning: bool
 var camera_offset: Vector2
 var original_camera_pos: Vector2
 var camera_moved_so_far: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	mousePositionLastFrame = mousePosition
+
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	mousePosition = get_global_mouse_position()
+	queue_redraw()
+	
+	# determine animation state
+	if panning:
+		animator.play("panning")
+	elif grabbing:
+		animator.play("grabbing")
+	else:
+		animator.play("idle")
+	
+	
+	
+	
+	# Move the camera when panning
+	mousePosition = get_global_mouse_position()
+	if panning:
+		
+		camera_moved_so_far = camera.global_position - original_camera_pos
+		var distanceToMove = ((get_global_mouse_position() - camera_moved_so_far) + camera_offset) - original_camera_pos
+		camera.global_position = original_camera_pos - distanceToMove * panning_sensitivity
 	
 	
 	# Keeping within a certain distance of the mouse cursor
@@ -40,34 +62,27 @@ func _process(_delta: float) -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	#if !dragging:
+	#if !panning:
 	position = position + (mousePosition - global_position) / handStiffness
 	
-	# Move the camera when dragging
-	mousePosition = get_global_mouse_position()
-	if dragging:
-		
-		camera_moved_so_far = camera.global_position - original_camera_pos
-		var distanceToMove = ((get_global_mouse_position() - camera_moved_so_far) + camera_offset) - original_camera_pos
-		camera.global_position = original_camera_pos - distanceToMove * panning_sensitivity
-		
-	
-	mousePositionLastFrame = get_global_mouse_position()
-	positionLastFrame = global_position
 
-	pass
+	
+
+	
 
 
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("Move")):
-		dragging = true
+		panning = true
 		camera_offset = camera.global_position - get_global_mouse_position()
 		original_camera_pos = camera.global_position
-	if(event.is_action_released("Move")): dragging = false
-
+	if(event.is_action_released("Move")): panning = false
+	
+	if(event.is_action_pressed("Grab")): grabbing = true
+	if(event.is_action_released("Grab")): grabbing = false
 
 func _draw() -> void:
-	#draw_circle(Vector2(0,0),maxDistanceFromMouse,Color.INDIAN_RED,false,2)
+	draw_circle(Vector2(0,0),maxDistanceFromMouse,Color.INDIAN_RED,false,2)
 	pass
 	
 func get_distance_to(target: Vector2):
