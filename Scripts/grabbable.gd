18d8f2399f4da3392a2ecca45554f53c795a4f31
RedFoxLightning <57427142+RedFoxLightning@users.noreply.github.com>
2026-03-02 17:21:49 -0800

    Added art, fixed current dragging implimentation
    Added art for goober ^^
    
    Also fixed the dragging implimentation to allow for more than one draggable thing, so long as each grabbable node is the child to a rigidbody, and has a box collisionShape2D as a child
    
    also added vignette
    
    Also turned off anti-aliasing

--- Scripts/grabbable.gd
+++ Scripts/grabbable.gd
@@ -1,7 +1,10 @@
-extends CollisionShape2D
+extends Node2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
+@onready var rigid_body: RigidBody2D = $".."
 var grabbed: bool = false;
 var relative_position;
+var pos;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,24 +13,28 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	setPosToRigidBodyPos()
 	if grabbed:
-		global_position = get_global_mouse_position() + relative_position
+		rigid_body.global_position = get_global_mouse_position() + relative_position
 	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Grab"):
-		var size: Vector2 = shape.size
+		var size: Vector2 = collision_shape.shape.size
 		var mouse: Vector2 = get_global_mouse_position()
+		setPosToRigidBodyPos()
 		
-		if (mouse.x < global_position.x + size.x / 2) and (mouse.x > global_position.x - size.x / 2):
-			if (mouse.y < global_position.y + size.y / 2) and (mouse.y > global_position.y - size.y / 2):
+		if (mouse.x < pos.x + size.x / 2) and (mouse.x > pos.x - size.x / 2):
+			if (mouse.y < pos.y + size.y / 2) and (mouse.y > pos.y - size.y / 2):
 				print("click!");
-				relative_position = global_position - get_global_mouse_position() 
+				relative_position = pos - get_global_mouse_position() 
 				grabbed = true
 	if event.is_action_released("Grab"):
 		grabbed = false 
+		print("unclick!");
 
-
+func setPosToRigidBodyPos():
+	pos = rigid_body.global_position;
 
 func _on_button_down() -> void:
 	grabbed = true
