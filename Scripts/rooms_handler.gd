d62a21044140210c6df13fb15ecf8be1aed8514b
RedFoxLightning <57427142+RedFoxLightning@users.noreply.github.com>
2026-03-06 16:04:10 -0800

    RoomHandler can now gen rooms!
    WOoo!
    Currently only the starting room generates
    Also take note that in order to change the area properties you'll want to change them inside the RoomHandler node found in the "main" scene, as the list is per instance

--- Scripts/rooms_handler.gd
+++ Scripts/rooms_handler.gd
@@ -1,11 +1,60 @@
 extends Node2D
 @export var areas: Array[AreaSO]
+var rng := RandomNumberGenerator.new()
+var base_state
+
+var room_numbers: Dictionary[Vector2, int] # used for adding to the seed ^^
+var latest_room_number := 0
+var room_areas: Dictionary[Vector2, AreaSO]
+var cleared: Dictionary[Vector2, bool]
+var loaded_rooms: Dictionary[Vector2, PackedScene]
+
+const room_size: Vector2 = Vector2(640,360)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(areas[0].GetRandomRoom())
+	base_state = rng.state 
+	#rng.seed(base_seed + 1)
 
+	var currentPos := Vector2(0,0)
+	for a in areas.size():
+		for r in areas[a].rooms.size():
+			
+			AssignNewRoomNumberAt(currentPos)
+			room_areas[currentPos] = areas[a]
+			
+			if !areas[a].vertical:
+				currentPos.x += 1
+			else:
+				# Reminder, +Y is DOWN
+				currentPos.y += 1 
+			#print(areas[a].rooms[r])
+	
+	#areas[0].GetRandomRoom()
+	GenerateRoomAt(Vector2(0,0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+	#print(rng.seed)
+
+func AssignNewRoomNumberAt(pos: Vector2):
+	room_numbers[pos] = latest_room_number
+	latest_room_number += 1
+	 
+func GenerateRoomAt(pos: Vector2):
+	
+	# get to the same exact same rng state wherever the room is 
+	var room_number = room_numbers[pos]
+	rng.state = base_state
+	for i in room_number:
+		rng.randi_range(0,0)
+	
+	
+	
+	var newRoom = room_areas[pos].GetRandomRoom()
+	newRoom = newRoom.instantiate()
+	add_child(newRoom)
+	newRoom.position = pos * room_size
+	
 	pass
